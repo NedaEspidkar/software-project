c9d0299666991a6b7f7b5f4f676433b7cb6c02a1
NedaEspidkar <n.spkr1991@gmail.com>
2016-11-25 21:20:07 +0330

    Add files via upload

---
+++ Courses.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Objects
+{
+    public class Courses
+    {
+	private int _ID;
+        public int ID
+        {
+            get
+            {
+                return _ID;
+            }
+            set
+            {
+                _ID = value;
+            }
+        }
+	private int _CourseID;
+        public int CourseID
+        {
+            get
+            {
+                return _CourseID;
+            }
+            set
+            {
+                _CourseID = value;
+            }
+        }
+	private int _TrainID;
+        public int TrainID
+        {
+            get
+            {
+                return _TrainID;
+            }
+            set
+            {
+                _TrainID = value;
+            }
+        }
+	private string _CourseName;
+        public string CourseName
+        {
+            get
+            {
+                return _CourseName;
+            }
+            set
+            {
+                _CourseName = value;
+            }
+        }
+	private string _ProfessorName;
+        public string ProfessorName
+        {
+            get
+            {
+                return _ProfessorName;
+            }
+            set
+            {
+                _ProfessorName = value;
+            }
+        }
+	private DateTime _ClassTime;
+        public DateTime ClassTime
+        {
+            get
+            {
+                return _ClassTime;
+            }
+            set
+            {
+                _ClassTime = value;
+            }
+        }
+       
+    }
+}
+
---
+++ Students.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Objects
+{
+    public class Students
+    {
+	private int _ID;
+        public int ID
+        {
+            get
+            {
+                return _ID;
+            }
+            set
+            {
+                _ID = value;
+            }
+        }
+       
+        private string _StudentID;
+        public string StudentID
+        {
+            get
+            {
+                return _StudentID;
+            }
+            set
+            {
+                _StudentID = value;
+            }
+        }
+
+	private string _StudentName;
+        public string StudentName
+        {
+            get
+            {
+                return _StudentName;
+            }
+            set
+            {
+                _StudentName = value;
+            }
+        }
+        
+	private string _StudentFamily;
+        public string StudentFamily
+        {
+            get
+            {
+                return _StudentFamily;
+            }
+            set
+            {
+                _StudentFamily = value;
+            }
+        }
+
+	private string _StudentPhone;
+        public string StudentPhone
+        {
+            get
+            {
+                return _StudentPhone;
+            }
+            set
+            {
+                _StudentPhone = value;
+            }
+        }
+
+	private string _StudentAddress;
+        public string StudentAddress
+        {
+            get
+            {
+                return _StudentAddress;
+            }
+            set
+            {
+                _StudentAddress = value;
+            }
+        }
+
+	private string _StudentGender;
+        public string StudentGender
+        {
+            get
+            {
+                return _StudentGender;
+            }
+            set
+            {
+                _StudentGender = value;
+            }
+        }
+
+	private int _TrainID;
+        public int TrainID;
+        {
+            get
+            {
+                return _TrainID;
+            }
+            set
+            {
+                _TrainID;= value;
+            }
+        }
+    }
+}
+
